9f14e96755ab218ca23b4f24a3154c6d5a4804ea
Andrew <andrewlian345@gmail.com>
2026-03-21 00:24:22 -0400

    add more tests for human player and command pattern

--- src/test/java/team8/catan/suites/GameplaySuite.java
+++ src/test/java/team8/catan/suites/GameplaySuite.java
@@ -4,17 +4,23 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 import team8.catan.gameplay.GameFactoryTest;
 import team8.catan.gameplay.GameTest;
+import team8.catan.gameplay.commands.BuildCityCommandTest;
 import team8.catan.gameplay.commands.CommandHistoryTest;
 import team8.catan.gameplay.commands.ResourceDistributionCommandTest;
+import team8.catan.logging.ConsoleActionLoggerTest;
+import team8.catan.players.HumanPlayerTest;
 import team8.catan.players.RandomAgentTest;
 import team8.catan.players.strategy.ImmediateValueSelectionPolicyTest;
 
 @Suite
 @SelectClasses({
+    BuildCityCommandTest.class,
     CommandHistoryTest.class,
     ResourceDistributionCommandTest.class,
+    HumanPlayerTest.class,
     ImmediateValueSelectionPolicyTest.class,
     RandomAgentTest.class,
+    ConsoleActionLoggerTest.class,
     GameTest.class,
     GameFactoryTest.class
 })
